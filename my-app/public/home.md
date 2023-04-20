d6521873021c3fd949527dd6854792d9ef2f47b5
Otavio Martins <otaviovacari@usp.br>
2023-04-20 16:50:04 -0300

    New css style

--- my-app/public/home.md
+++ my-app/public/home.md
@@ -1,2 +1,8 @@
-# home
-Ola mundo
+# What we do?
+We are a learning plataform! </br>
+You subscribe and receive the course via email.
+
+## Similar project
+
+The project where i first saw ( and used ) e e-mail course was on webpack, a really weird an complex module on JavaScript. Some how it workded for me! So i decided to make it similar.
+https://createapp.dev/webpack-course
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import { marked } from 'marked';
+import { HiAcademicCap } from 'react-icons/hi'
+import { BiCodeBlock, BiHardHat } from 'react-icons/bi'
+import { FiAlertTriangle } from 'react-icons/fi'
 
 export async function getStaticProps() {
   const metadataUrl = new URL('/post/data.json', "http://localhost:3000");
   const metadataResponse = await fetch(metadataUrl);
   const markdownList = await metadataResponse.json();
-  
+
   const markdownHomeURL = await new URL('/home.md', "http://localhost:3000");
   const metadataHomeResponse = await fetch(markdownHomeURL);
   const markdownHomeText = await metadataHomeResponse.text();
@@ -19,26 +22,59 @@ export async function getStaticProps() {
       htmlHome: htmlHome,
     },
   };
+  
 }
-
 
 
 export default function HomePage({ markdownList, htmlHome }) {
   return (
-    <div className="first-div">
-      <h1>PLaylists</h1>
-      <div dangerouslySetInnerHTML={{ __html: htmlHome }} />
-      <ul>
-        {markdownList.map(({ playlistTitle, id, descricaoPlaylist }) => (
-          <li key={id}>
-            <Link href={`/post/${id}`}>
-                <h2>{playlistTitle}</h2>
-            </Link>
-            <p>id:{id}</p>
-            <p>{descricaoPlaylist}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <header>
+        <h3>The Engineer&apos;s guide to blockchain</h3>
+        <p>We help undergrad stutens to <mark>learn</mark> blockchain</p>
+        <br />
+        <h1>Learning paths</h1>
+        <div>
+          Learn about <b>research</b> or about <b>web development</b>
+          <section>
+            <aside>
+              < HiAcademicCap size={100} />
+              <h3>Academic</h3>
+              <p>Wanna <mark>read</mark> more articles and papers? Read papers and see what will be build in the next years!</p>
+              <p><small>*We will focus more on understand than actualy build</small></p>
+            </aside>
+            <aside>
+              <BiCodeBlock size={100} />
+              <h3>Developer</h3>
+              <p>So you wanna <mark>build</mark> on web3? Learn blockchain concepts while build on it!</p>
+              <p><small>If you like getting your hands dirty, this is for you</small></p>
+            </aside>
+          </section>
+        </div>
+      </header>
+      <main>
+        <div dangerouslySetInnerHTML={{ __html: htmlHome }} />
+        <details>
+          <summary><BiHardHat /><FiAlertTriangle /></summary>
+          <ul>
+            {markdownList.map(({ playlistTitle, id, descricaoPlaylist }) => (
+              <li key={id}>
+                <Link href={`/post/${id}`}>
+                  <h2>{playlistTitle}</h2>
+                </Link>
+                <p>id:{id}</p>
+                <p>{descricaoPlaylist}</p>
+              </li>
+            ))}
+          </ul>
+        </details>
+      </main>
+      <footer>
+        <hr />
+        <p>
+          <small>otavio.vac@gmail.com</small>
+        </p>
+      </footer>
     </div>
   );
 }
