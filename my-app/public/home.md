2c246376306d8fd8c021c57d08dcad4959700ffe
Otavio Martins <otaviovacari@usp.br>
2023-04-21 12:49:18 -0300

    new front end

--- my-app/public/home.md
+++ my-app/public/home.md
@@ -4,5 +4,5 @@ You subscribe and receive the course via email.
 
 ## Similar project
 
-The project where i first saw ( and used ) e e-mail course was on webpack, a really weird an complex module on JavaScript. Some how it workded for me! So i decided to make it similar.
+The project where i first saw ( and used ) e-mail course was on webpack, a really weird an complex module on JavaScript. Some how it workded for me! So i decided to make it similar.
 https://createapp.dev/webpack-course
--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -31,17 +31,25 @@ export default function HomePage({ markdownList, htmlHome }) {
     <div>
       <header>
         <h3>The Engineer&apos;s guide to blockchain</h3>
-        <p>We help undergrad stutens to <mark>learn</mark> blockchain</p>
+        <p>We help undergrad students to <mark>learn</mark> blockchain</p>
         <br />
         <h1>Learning paths</h1>
         <div>
           Learn about <b>research</b> or about <b>web development</b>
           <section>
+          <aside>
+              <BiCodeBlock size={100} />
+              <h3>Beginner</h3>
+              <p>So you wanna <mark>build</mark> on web3? Learn blockchain concepts while build on it!</p>
+              <p><small>If you like getting your hands dirty, this is for you</small></p>
+            </aside>
+          </section>
+          <section>
             <aside>
               < HiAcademicCap size={100} />
               <h3>Academic</h3>
               <p>Wanna <mark>read</mark> more articles and papers? Read papers and see what will be build in the next years!</p>
-              <p><small>*We will focus more on understand than actualy build</small></p>
+              <p><small>We will focus more on understand than actualy build</small></p>
             </aside>
             <aside>
               <BiCodeBlock size={100} />
